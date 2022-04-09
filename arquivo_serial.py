4a4cde196d310c5d89f09e45098c06f89ef54d4b
bruno1604 <brunont1211@gmail.com>
2022-04-08 23:27:08 -0300

    Finalizando interface de envio e leitura dos dados da porta serial

--- arquivo_serial.py
+++ arquivo_serial.py
@@ -23,17 +23,53 @@ def LeituraSerial():
 
     #Cria uma variável que exibirá o que foi lido na porta serial
     RetornoLeituraSerial = Label(janela, text=valor_recebido)
-    RetornoLeituraSerial.place(x=200,y=100)
+    RetornoLeituraSerial.place(x=740,y=112)
+    RetornoLeituraSerial['bg'] = 'light blue'
+
+def EnviarDados():
+    print(EnviarDadosSerial.get())
+    try:
+        ser = serial.Serial('COM3', 9600)
+        while True:
+            ser.write(EnviarDadosSerial.get())
+            if ser.inWaiting() > 0:
+                ser.close()
+                break
+    except:
+        print("Erro ao enviar mensagem")
+        MensagemErroEnvio = Label(janela, text="Erro ao enviar mensagem para a porta serial")
+        MensagemErroEnvio.place(x=550,y=230)
+        MensagemErroEnvio['bg'] = 'light blue'
+
 
 #cria uma janela Tkinter
 janela = tk.Tk()
+janela.title("Leitura e envio de dados para a porta Serial")
+janela['bg'] = 'light blue'
 
 #define parâmetros de largura e altura da janela
 janela.geometry("1300x700+0+0")
 
+TituloLerPortaSerial = Label(janela, text="Leitura da Porta Serial", font=('calibri 18'))
+TituloLerPortaSerial.place(x=550, y=50)
+TituloLerPortaSerial['bg'] = 'light blue'
+
 #Cria um botão que chamará a função LeituraSerial ao ser clicado
-LerPortaSerial = Button(janela, text="Ler porta serial", command=LeituraSerial)
-LerPortaSerial.place(x=100, y=100)
+LerPortaSerial = Button(janela, text="Ler porta serial", command=LeituraSerial, width=25)
+LerPortaSerial.place(x=550, y=110)
+LerPortaSerial['bg'] = '#00FF7F'
+
+TituloEnviarDadosPortaSerial = Label(janela, text="Leitura da Porta Serial", font=('calibri 18'))
+TituloEnviarDadosPortaSerial.place(x=550, y=50)
+TituloEnviarDadosPortaSerial['bg'] = 'light blue'
+
+EnviarDadosSerial = Entry(janela, width=25)
+EnviarDadosSerial.place(x=550, y=200)
+
+BotaoEnviarDadosSerial = Button(janela, text="Enviar dados", command=EnviarDados)
+BotaoEnviarDadosSerial.place(x=710,y=197)
+BotaoEnviarDadosSerial['bg'] = '#00FF7F'
+
 
 #Cria um loop na janela para ela não ser fechada automaticamente
 janela.mainloop()
